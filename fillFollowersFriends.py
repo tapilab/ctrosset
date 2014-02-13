ebca642adc799fbd1672cf3ced02614f9cbffc8d
Cyril Trosset <cyriltrosset@MacBook-Pro-de-Cyril-2.local>
2014-02-12 18:37:09 -0600

    Optimization

--- fillFollowersFriends.py
+++ fillFollowersFriends.py
@@ -37,76 +37,64 @@ print "OK"
 print "Looking for the first row not treated ..."
 
 con = lite.connect(DATABASE)
-with con:
-	con.row_factory = lite.Row
-	
-	file = open(FILE, 'r')
-	treatedInFile = file.read()
-	file.close()
-	
-	cur = con.cursor()
-#	cur.execute("SELECT * FROM Profiles LIMIT "+treatedInFile+",200")
- 	cur.execute("SELECT Profiles.idFollower FROM Profiles LEFT OUTER JOIN Users on Profiles.idFollower = Users.idUser WHERE Users.idUser is null")
-	rows = cur.fetchall()
-	
-	currentAccount = 0
-	
-	iRow = 1
-	treated = int(treatedInFile)
-	treatedSave = 0
-	
-	for row in rows:
-		cur2 = con.cursor()
-		cur2.execute("SELECT Count(*) FROM Users WHERE idUser=" + str(row["idFollower"]))
-		
-		if(treated>=0):
-			treated+=1
-		
-		numberOfRows = cur2.fetchone()[0]
-		if(numberOfRows==0):
-			if(iRow==1 and treated>=0):
-				print 'OK, ' + str(treated) + ' rows already treated'
-				treatedSave = treated
-				treated = -1
-			try:
-				error = False
-				friends = twitter.get_friends_ids(user_id = row["idFollower"])
-			except TwythonAuthError:
-				error = True
-				cur4 = con.cursor()
-				cur4.execute("DELETE FROM Users WHERE idUser="+str(row["idFollower"]))
-				cur4.execute("DELETE FROM Profiles WHERE idFollower="+str(row["idFollower"]))
-			except TwythonRateLimitError:
-				error = True
-				currentAccount+=1
-				if currentAccount == len(APP_KEY):
-					print 'Limit excedeed and all accounts have been used, quitting ...'			
-					
-					file = open(FILE,'w')
-					print str(treatedSave+iRow)
-					file.write(str(treatedSave+iRow-5))
-					file.close()
-					sys.exit()
-				else:
-					print 'Switching account ...'
-					twitter = Twython(APP_KEY[currentAccount], APP_SECRET[currentAccount], oauth_version=2)
-					ACCESS_TOKEN = twitter.obtain_access_token()
-					twitter = Twython(APP_KEY[currentAccount], access_token=ACCESS_TOKEN)
-					print 'OK'
-			except TwythonError as (e):
-				error = True
-				cur4 = con.cursor()
-				cur4.execute("DELETE FROM Users WHERE idUser="+str(row["idFollower"]))
-				cur4.execute("DELETE FROM Profiles WHERE idFollower="+str(row["idFollower"]))
 
-			if 'friends' in locals():
-				for friends_id in friends['ids']:
-					with con:
-						cur3 = con.cursor()
-						cur3.execute("INSERT INTO Users VALUES('" + str(row["idFollower"]) + "','" + str(friends_id) + "')")
+con.row_factory = lite.Row
+	
+file = open(FILE, 'r')
+treatedInFile = file.read()
+file.close()
+	
+cur = con.cursor()
+cur.execute("SELECT Profiles.idFollower FROM Profiles LEFT OUTER JOIN Users on Profiles.idFollower = Users.idUser WHERE Users.idUser is null LIMIT 0,100")
+rows = cur.fetchall()
+	
+currentAccount = 0
+	
+iRow = 1
+	
+for row in rows:
+    cur2 = con.cursor()
+    cur2.execute("SELECT Count(*) FROM Users WHERE idUser=" + str(row["idFollower"]))
 		
-			if(error==False):				
-				iRow+=1
-				print "Row " + str(iRow) + " treated"
-				
+    numberOfRows = cur2.fetchone()[0]
+    if(numberOfRows==0):
+        try:
+            error = False
+            friends = twitter.get_friends_ids(user_id = row["idFollower"])
+        except TwythonAuthError:
+            error = True
+            cur4 = con.cursor()
+            cur4.execute("DELETE FROM Users WHERE idUser="+str(row["idFollower"]))
+            cur4.execute("DELETE FROM Profiles WHERE idFollower="+str(row["idFollower"]))
+        except TwythonRateLimitError:
+            error = True
+            currentAccount+=1
+            if currentAccount == len(APP_KEY):
+                print 'Limit excedeed and all accounts have been used, quitting ...'
+                
+                con.commit()
+                con.close()
+                sys.exit()
+            else:
+                print 'Switching account ...'
+                twitter = Twython(APP_KEY[currentAccount], APP_SECRET[currentAccount], oauth_version=2)
+                ACCESS_TOKEN = twitter.obtain_access_token()
+                twitter = Twython(APP_KEY[currentAccount], access_token=ACCESS_TOKEN)
+                print 'OK'
+        except TwythonError as (e):
+            error = True
+            cur4 = con.cursor()
+            cur4.execute("DELETE FROM Users WHERE idUser="+str(row["idFollower"]))
+            cur4.execute("DELETE FROM Profiles WHERE idFollower="+str(row["idFollower"]))
+
+        if 'friends' in locals():
+            for friends_id in friends['ids']:
+                    cur3 = con.cursor()
+                    cur3.execute("INSERT INTO Users VALUES('" + str(row["idFollower"]) + "','" + str(friends_id) + "')")
+		
+        if(error==False):
+            iRow+=1
+            print "Row " + str(iRow) + " treated"
+
+con.commit()
 con.close()
