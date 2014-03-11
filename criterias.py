fcaded965913beb04bf23b38d810b6a5aae2e2c5
Cyril Trosset <cyriltrosset@MacBook-Pro-de-Cyril-3.local>
2014-03-11 18:17:00 -0500

    Duplicate code clean + Cross validation tests

--- criterias.py
+++ criterias.py
@@ -1,26 +1,13 @@
 import sqlite3 as lite
 import sys
 from scipy.sparse import lil_matrix
+import loadConfig
 
+FILE = ''
+DATABASE = ''
 APP_KEY = {}
 APP_SECRET = {}
-
-foobar = open( "foobar.config", "r" )
-
-i=0
-for line in foobar:
-    line=line.rstrip('\n')
-    if i==0:
-        FILE = line
-    elif i==1:
-        DATABASE = line
-    elif i==2:
-        APP_KEY = line.split(',')
-    elif i==3:
-        APP_SECRET = line.split(',')
-    i+=1
-    
-foobar.close()
+loadConfig.loadConfig(FILE,DATABASE,APP_KEY,APP_SECRET)
 
 con = lite.connect(DATABASE)
     
--- fillFollowersFriends.py
+++ fillFollowersFriends.py
@@ -3,29 +3,13 @@
 from twython import Twython, TwythonError, TwythonAuthError, TwythonRateLimitError
 import sqlite3 as lite
 import sys
+import loadConfig
 
 FILE = ''
 DATABASE = ''
-
 APP_KEY = {}
 APP_SECRET = {}
-
-foobar = open( "/home/ctrosset/ctrosset/foobar.config", "r" )
-
-i=0
-for line in foobar:
-    line=line.rstrip('\n')
-    if i==0:
-        FILE = line
-    elif i==1:
-        DATABASE = line
-    elif i==2:
-        APP_KEY = line.split(',')
-    elif i==3:
-        APP_SECRET = line.split(',')
-    i+=1
-    
-foobar.close()
+loadConfig.loadConfig(FILE,DATABASE,APP_KEY,APP_SECRET)
 
 print "Connecting ..."
 twitter = Twython(APP_KEY[0], APP_SECRET[0], oauth_version=2)
