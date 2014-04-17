31257fb99f67c22e973f7e0b2149d52c5f2ffacb
Cyril Trosset <cyriltrosset@MacBook-Pro-de-Cyril-3.local>
2014-04-17 17:39:39 +0200

    Cross validation plot

--- loadCrossValidationFromPickle.py
+++ loadCrossValidationFromPickle.py
@@ -21,7 +21,7 @@ APP_SECRET = {}
 
 loadConfig.loadConfig(FILE,DATABASE,APP_KEY,APP_SECRET)
 
-
+"""
 print "Loading matrix X ..."
 
 f = open('X.pkl','rb') # open the file in read binary mode
@@ -42,32 +42,28 @@ print "Y matrix loaded"
 
 
 Xd = X.todense()
-Yd = Y.todense()
-clf = Ridge(alpha=0.1)
+Yd = Y[:,0].todense()
+"""
+
+clf = Ridge(alpha=1)
 cv = cross_validation.KFold(len(Yd), n_folds=10, random_state=1234)
+
+predicted_values = []
+true_values = []
+
 for train, test in cv:
 	print 'train indices:', train
 	print 'test indices:', test
 	clf.fit(Xd[train], Yd[train])
-	preds = clf.predict(Xd[test])
+	preds = clf.predict(Xd[test])	
+	predicted_values.extend(preds[:,0])
+	true_values.extend(np.squeeze(np.asarray(Yd[test]))) 
 	
 
 #PLOT PREDICTED VS "TRUE"
-predict_table = clf.predict(X)
-n_groups = predict_table.shape[0]
-
-predicted_values = []
-true_values = []
-x = []
-
-for i in range(0,n_groups):
-    predicted_values.append(round(predict_table[i][0]))
-    true_values.append(round(Y[i,0]))
-    x.append(i)
-    
 
 plot(predicted_values,true_values,'.')
-plot(x[0:100],x[0:100],'r',color='black')
+plot(range(0,100),range(0,100),'r',color='black')
 xlabel('% of Male Predicted')
 ylabel('% of Male True')
 title('True vs Predicted')
